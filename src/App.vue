22c573e791508f7a87c8de4321b5d41824bc6427
DidierDankan <didi_archer@hotmail.com>
2021-05-04 14:43:15 +0100

    scafolding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Header -->
+
+
+    <!-- Main -->
+
+
+    <!-- Footer -->
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import headerDC from '@/components/headerDC.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    headerDC
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ src/components/headerDC.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name:'headerDC'
+}
+</script>
+
+<style scoped>
+
+</style>
